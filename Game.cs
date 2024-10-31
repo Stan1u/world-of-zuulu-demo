55a0cfb0b02954b49e40931cc598b39be479e969
Patryk <staniu62@gmail.com>
2024-10-31 11:09:27 +0100

    Equipment, rooms lock, theatre

---
+++ Game.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace WorldOfZuul
+{
+    public class Game
+    {
+        private Room? currentRoom;
+        private Room? previousRoom;
+        private Inventory_functionality inventory;
+
+        public Game()
+        {
+            CreateRooms();
+            inventory = new Inventory_functionality();
+
+            inventory.AddItem(new Item("house key", "just a key to Ur house"));
+        }
+
+        private void CreateRooms()
+        {
+            Room? outside = new("Outside", "You are standing outside the main entrance of the university.");
+            Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. You can See a KEVIN LE MINION screaming for help!", "Emerald");
+            Room? pub = new("Pub", "You've entered the campus pub.");
+            Room? lab = new("Lab", "You're in a computing lab." , "", "Emerald" );
+            Room? office = new("Office", "You've entered an administration office.");
+
+            outside.SetExits(null, theatre, lab, pub);
+            theatre.SetExit("west", outside);
+            pub.SetExit("east", outside);
+            lab.SetExits(outside, office, null, null);
+            office.SetExit("west", lab);
+
+            currentRoom = outside;
+        }
+
+        public void Play()
+        {
+            Parser parser = new();
+            PrintWelcome();
+            bool continuePlaying = true;
+
+            while (continuePlaying)
+            {
+                Console.WriteLine(currentRoom?.ShortDescription);
+                Console.Write("> ");
+
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Please enter a command.");
+                    continue;
+                }
+
+                Command? command = parser.GetCommand(input);
+
+                if (command == null)
+                {
+                    Console.WriteLine("I don't know that command.");
+                    continue;
+                }
+
+                switch (command.Name)
+                {
+                    case "look":
+                        Console.WriteLine(currentRoom?.LongDescription);
+                        break;
+
+                    case "back":
+                        if (previousRoom == null)
+                            Console.WriteLine("You can't go back from here!");
+                        else
+                            currentRoom = previousRoom;
+                        break;
+                    
+                    case "talk":
+                        switch (currentRoom.ShortDescription)
+                        {
+                            case ("Outsie"):
+                                Console. WriteLine("You talk to urself madman?!");
+                                break;
+                                 case("Theatre"):
+                                Console.WriteLine(
+                                    "Kevin wants a banana but in exchange for it he has to give the villagers some clear water. How do we get it?\n" +
+                                    "1. From the tap that u have in ur house\n" +
+                                    "2. Get water from the river and filter it\n" +
+                                    "3. Give them a salty one, no one will realize anyway");
+                                string chooise = Console.ReadLine();
+                                switch (chooise)
+                                {
+                                    case("1"):
+                                        Console.WriteLine("You helped! Kevin got Banna and gives u an emerald!");
+                                        Item emerald = new Item("Emerald", "You are standing emerald.");
+                                        inventory.AddItem(emerald);
+                                        currentRoom = previousRoom;
+
+                                        break;
+                                    case("2"):
+                                        Console.WriteLine("Vills are half happy cuz they could've done it themselves!U recive some red powder");
+                                        Item powder = new Item("Red Powder", "You are standing red powder.");
+                                        inventory.AddItem(powder);
+                                        currentRoom = previousRoom;
+
+                                        break;
+                                    case("3"):
+                                        Console.WriteLine("No way u tought that's gonna work out! Get the hell out of here before the Golem kills U");
+                                        currentRoom = previousRoom;
+                                        break;
+                        }
+
+                    break;
+                                 case("lab"):
+                                     
+                                     
+                                     break;
+                         }
+
+                        break;
+
+                    case "north":
+                    case "south":
+                    case "east":
+                    case "west":
+                        Move(command.Name);
+                        break;
+
+                    case "quit":
+                        continuePlaying = false;
+                        break;
+
+                    case "help":
+                        PrintHelp();
+                        break;
+
+                    case "show":
+                        inventory.ShowItems();
+                        break;
+
+                    default:
+                        Console.WriteLine("I don't know what command.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Thank you for playing World of Zuul!");
+        }
+
+        private void Move(string direction)
+        {
+            if (currentRoom?.Exits.ContainsKey(direction) == true)
+            {
+                if (currentRoom.Exits[direction].Check(inventory))
+                {
+                    previousRoom = currentRoom;
+                    currentRoom = currentRoom?.Exits[direction];
+                }
+            }
+            else
+            {
+                Console.WriteLine($"You can't go {direction}!");
+            }
+        }
+
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the World of Zuul!");
+            Console.WriteLine("World of Zuul is a new, incredibly boring adventure game.");
+            PrintHelp();
+            Console.WriteLine();
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("You are lost. You are alone. You wander around the university.");
+            Console.WriteLine();
+            Console.WriteLine("Navigate by typing 'north', 'south', 'east', or 'west'.");
+            Console.WriteLine("Type 'look' for more details.");
+            Console.WriteLine("Type 'back' to go to the previous room.");
+            Console.WriteLine("Type 'help' to print this message again.");
+            Console.WriteLine("Type 'quit' to exit the game.");
+            Console.WriteLine("Type 'show' to display your inventory.");
+        }
+
+       
+
+            }
+        }
+    
---
+++ Room.cs
@@ -0,0 +1,92 @@
+﻿namespace WorldOfZuul
+{
+    public class Room
+    {
+        public string ShortDescription { get; private set; }
+        public string LongDescription { get; private set;}
+        public Dictionary<string, Room> Exits { get; private set; } = new();
+        public string reqired_item { get; private set; }
+        public string dispise_item { get; private set; }
+        public Room( string shortDesc, string longDesc, string dispise_item = null, string reqired_item = null)
+        {
+            ShortDescription = shortDesc;
+            LongDescription = longDesc;
+            this.reqired_item = reqired_item;
+            this.dispise_item = dispise_item;
+        }
+
+        public void SetExits(Room? north, Room? east, Room? south, Room? west)
+        {
+            SetExit("north", north);
+            SetExit("east", east);
+            SetExit("south", south);
+            SetExit("west", west);
+        }
+
+        public void SetExit(string direction, Room? neighbor)
+        {
+            if (neighbor != null)
+                Exits[direction] = neighbor;
+        }
+
+        
+        public bool Check(Inventory_functionality inventory)
+        {
+            if (string.IsNullOrEmpty(reqired_item) && string.IsNullOrEmpty(dispise_item))
+            {
+                return true;
+            }
+
+            else if (!string.IsNullOrEmpty(dispise_item))
+            {
+                foreach (Item item in inventory.items_holder)
+                {
+                    if (item.Name == dispise_item)
+                    {
+                        Console.WriteLine("You can't enter back");
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            Console.WriteLine("A Villager stoped You! He wants an Emerald if you want him to let you pass\n"
+                              + "1. Give him emerald\n"
+                              + "2. Go Back\n");
+            
+
+            while (true)
+            {
+                string chooise2 = Console.ReadLine();
+                if (chooise2 == "1")
+                {
+                    foreach (Item item in inventory.items)
+                    {
+                        if (item.Name == reqired_item)
+                        {
+                            Console.WriteLine("You entered ");
+                            inventory.GiveItem(item);
+                            return true;
+                        }
+                    }
+
+                    Console.WriteLine("You can't enter ");
+                    return false;
+
+
+                }
+                else if (chooise2 == "2")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Choose between 1 and 2");
+                }
+            }
+        }
+
+    }
+    
+    
+}
