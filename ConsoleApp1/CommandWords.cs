47839987f7c007ffcc25150ccb2c109c984f4108
Volkan1200 <135809505+Volkan1200@users.noreply.github.com>
2024-12-31 18:49:34 +0200

    Add files via upload

---
+++ ConsoleApp1/CommandWords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldOfZuul
+{
+    public class CommandWords
+    {
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "explore", "back", "map", "quit", "help", "show", "talk", "1", "2", "3", "4" };
+
+        public bool IsValidCommand(string command)
+        {
+            return ValidCommands.Contains(command);
+        }
+    }
+
+}
