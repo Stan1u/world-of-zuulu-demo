6ab771bb6020f17edde1651317dd85affe8bb8e7
Sandra V <sandra.veretennikov@gmail.com>
2025-01-02 00:40:48 +0200

    some small changes

--- obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9dd56d0857de17cfedf003d61824c7bdbf480a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4bfa1a1c002b0dadc4632847b811066cec73f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
